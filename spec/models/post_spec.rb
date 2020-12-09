831b034714350ab53e77d396c406a5889ed47f1f
Kulebyakin Andrey <ankulebyakin@gmail.com>
2020-12-09 12:43:11 +0800

    Devide post spec to validations and associations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,13 @@
 require "rails_helper"
 
 describe Post do
-    it { should validate_presence_of :title }
-    it { should validate_presence_of :article }
-    it { should have_many :comments }
-    it { should belong_to :user }
+    describe 'validations' do
+        it { should validate_presence_of :title }
+        it { should validate_presence_of :article } 
+    end
+
+    describe 'association' do
+        it { should have_many :comments }
+        it { should belong_to :user }
+    end
 end
